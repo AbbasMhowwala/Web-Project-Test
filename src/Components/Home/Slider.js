bf57f776fe55f14313070bf51831ad5f36ac1eba
Abbas Mhowwala <89777973+AbbasMhowwala@users.noreply.github.com>
2022-03-21 17:14:56 +0530

    Slider changes has been done.

--- src/Components/Home/Slider.js
+++ src/Components/Home/Slider.js
@@ -5,17 +5,66 @@ import "react-awesome-slider/dist/styles.css";
 import slide1 from "../Assets/Images/slide1.jpg";
 import slide2 from "../Assets/Images/slide2.jpg";
 import slide3 from "../Assets/Images/slide3.jpg";
+import Carousel from 'react-bootstrap/Carousel'
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 const Slider = () => {
   return (
     <div className="hero-slider">
       <div className="hero-img">
         <Container fluid className="p-0">
           <Row className="m-auto w-100">
-            <AwesomeSlider bullets={false} className="slider-bw p-0">
+            {/* <AwesomeSlider bullets={false} className="slider-bw p-0">
               <div data-src={slide1} />
               <div data-src={slide2} />
               <div data-src={slide3} />
-            </AwesomeSlider>
+            </AwesomeSlider> */}
+            <Carousel fade className="p-0">
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={slide1}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h1 className="slider-title">Engagement Rings</h1>
+                  <p>
+                  Discover our unique Engagement Rings collection
+                  </p>
+                  <Button variant="contained" color="primary" className="slider-btn"><Link to="/">Shop Engagement Rings</Link></Button>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={slide2}
+                  alt="Second slide"
+                />
+                <Carousel.Caption>
+                  <h1 className="slider-title">Perfect Symbol of Love</h1>
+                  <p>
+                  Explore the latest Wedding collection
+                  </p>
+                  <Button variant="contained" color="primary" className="slider-btn"><Link to="/">Shop Wedding Bands</Link></Button>
+
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={slide3 }
+                  alt="Third slide"
+                />
+                <Carousel.Caption>
+                  <h1 className="slider-title">Fine Jewelry</h1>
+                  <p>
+                  Explore the latest Fine Jewelry
+                  </p>
+                  <Button variant="contained" color="primary" className="slider-btn"><Link to="/">Shop Fine Jewelry</Link></Button>
+
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
           </Row>
         </Container>
       </div>
